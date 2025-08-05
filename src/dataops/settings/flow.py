912460cf248d5372518f6a581469ed7e57485438
Alexander Senetcky <41960163+asenetcky@users.noreply.github.com>
2025-08-05 13:47:49 -0400

    feat: add app settings for data flow

---
+++ src/dataops/settings/flow.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from dataops.configs.account import AccountConfig
+from dataops.configs.acs import ACSConfig
+from dataops.configs.socrata import SocrataAPIConfig
+
+
+class AppSettings(BaseSettings):
+    """
+    Defines application settings for interacting with the portal platform and Census API.
+    """
+
+    model_config = SettingsConfigDict(env_file=".env", env_nested_delimiter="__")
+
+    census: ACSConfig
+    account: AccountConfig
+    api: SocrataAPIConfig
