7d94cc53e67edecdf2e9d41171bdd1c3607dee55
Alexander Senetcky <41960163+asenetcky@users.noreply.github.com>
2025-09-04 11:20:04 -0400

    feat: convert to wip adhoc model app

--- adhoc/adhoc-model.py
+++ adhoc/adhoc-model.py
@@ -12,13 +12,31 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md("""## Data Read""")
+    mo.md("""## Read-in Parquet""")
     return
 
 
 @app.cell
-def _(pl):
-    long_data = pl.scan_parquet("adhoc/adhoc.parquet")
+def _(mo):
+    data_file = mo.ui.file(filetypes=[".parquet"])
+    data_file
+    return (data_file,)
+
+
+@app.cell
+def _(mo):
+    mo.md("""Parquet Data Preview:""")
+    return
+
+
+@app.cell
+def _(data_file, pl):
+    if data_file.value:
+        long_data = pl.scan_parquet(data_file.contents())
+    else:
+        long_data = pl.LazyFrame()
+
+
     long_data.head().collect()
     return (long_data,)
 
@@ -37,145 +55,99 @@ def _(ACSStarModelBuilder, long_data):
 
 @app.cell
 def _(builder):
-    star = (
-        builder.set_stratifiers()
-        .set_concept()
-        .set_endpoint()
-        .set_valuetype()
-        .set_dataset()
-        .set_universe()
-        .set_health_indicator()
-        .set_fact()
-        .build()
-    )
+    star = False
+    try:
+        star = (
+            builder.set_stratifiers()
+            .set_concept()
+            .set_endpoint()
+            .set_valuetype()
+            .set_dataset()
+            .set_universe()
+            .set_health_indicator()
+            .set_fact()
+            .build()
+        )
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
     return (star,)
 
 
 @app.cell
 def _(star):
-    star.fact.collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_health_indicator.sort("health_indicator").collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_concept.collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_dataset.collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_endpoint.collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_stratifiers.collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_universe.collect()
-    return
-
-
-@app.cell
-def _(star):
-    star.dim_valuetype.collect()
-    return
-
-
-@app.cell
-def _(star):
-    huh = (
-        star.fact.group_by("DimHealthIndicatorID")
-        .len()
-        .sort(["len", "DimHealthIndicatorID"], descending=True)
-        .head()
-        .select("DimHealthIndicatorID")
-        .collect()
-        .to_series()
-        .to_list()
+    if star:
+        fact = star.fact
+        dim_health_indicator = star.dim_health_indicator
+        dim_concept = star.dim_concept
+        dim_dataset = star.dim_dataset
+        dim_endpoint = star.dim_endpoint
+        dim_stratifiers = star.dim_stratifiers
+        dim_universe = star.dim_universe
+        dim_valuetype = star.dim_valuetype
+    return (
+        dim_concept,
+        dim_dataset,
+        dim_endpoint,
+        dim_health_indicator,
+        dim_stratifiers,
+        dim_universe,
+        dim_valuetype,
+        fact,
     )
-    huh
-    return (huh,)
 
 
 @app.cell
-def _(huh, pl, star):
-    star.dim_health_indicator.filter(
-        pl.col("DimHealthIndicatorID").is_in(huh)
-    ).collect()
+def _(mo):
+    mo.md("""Fact:""")
     return
 
 
 @app.cell
-def _(huh, pl, star):
-    star.fact.filter(pl.col("DimHealthIndicatorID").is_in(huh)).collect()
-    return
-
-
-@app.cell
-def _(pl, star):
-    star.fact.filter(pl.col("DimHealthIndicatorID").eq(813)).filter(
-        pl.col("DimValueTypeID").eq(5)
-    ).collect()
+def _(fact, pl, star):
+    if star:
+        _head = fact.head(15).collect()
+    else:
+        _head = pl.DataFrame().head()
+    _head
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        "some of this stuff looks weird - but it might be better to let others take a look as well."
+    parquet_export_button = mo.ui.run_button(
+        tooltip="Export Model to parquet files.",
+        label="Export Model",
     )
-    return
+    parquet_export_button
+    return (parquet_export_button,)
 
 
 @app.cell
-def _(mo):
-    mo.md("TODO: add a dim_acs_variable etc...")
-    return
+def _(
+    dim_concept,
+    dim_dataset,
+    dim_endpoint,
+    dim_health_indicator,
+    dim_stratifiers,
+    dim_universe,
+    dim_valuetype,
+    fact,
+    parquet_export_button,
+):
+    if parquet_export_button.value:
+        print("Exporting model to parquet files")
 
+        fact.sink_parquet("fact.parquet")
+        dim_health_indicator.sink_parquet("dim_health_indicator.parquet")
+        dim_concept.sink_parquet("dim_concept.parquet")
+        dim_dataset.sink_parquet("dim_dataset.parquet")
+        dim_endpoint.sink_parquet("dim_endpoint.parquet")
+        dim_stratifiers.sink_parquet("dim_stratifiers.parquet")
+        dim_universe.sink_parquet("dim_universe.parquet")
+        dim_valuetype.sink_parquet("dim_valuetype.parquet")
 
-@app.cell
-def _(mo):
-    mo.md("## write it out")
-    return
-
-
-@app.cell
-def _(star):
-    star
-    return
-
-
-@app.cell(disabled=True)
-def _(star):
-    # TODO: add this as a method to builder
-    begin = "adhoc/adhoc_"
-    end = ".parquet"
-    star.fact.sink_parquet(f"{begin}fact{end}")
-    star.dim_stratifiers.sink_parquet(f"{begin}stratifiers{end}")
-    star.dim_universe.sink_parquet(f"{begin}universe{end}")
-    star.dim_concept.sink_parquet(f"{begin}concept{end}")
-    star.dim_valuetype.sink_parquet(f"{begin}valuetype{end}")
-    star.dim_health_indicator.sink_parquet(f"{begin}healthindicator{end}")
-    star.dim_endpoint.sink_parquet(f"{begin}endpoint{end}")
-    star.dim_dataset.sink_parquet(f"{begin}dataset{end}")
+        print("Files Exported")
     return
 
 
